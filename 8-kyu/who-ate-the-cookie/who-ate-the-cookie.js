63d99527f686196958ced059c6e58c964d50c60c
Marjorie Alouidor <alouidor.m@gmail.com>
2025-09-15 01:51:32 -0400

    Add solution - CodeHub

---
+++ 8-kyu/who-ate-the-cookie/who-ate-the-cookie.js
@@ -0,0 +1,16 @@
+function cookie(x){
+  if (typeof x === "string"){
+    return "Who ate the last cookie? It was Zach!";
+  } else if (typeof x === "number"){
+    return "Who ate the last cookie? It was Monica!";
+  } return "Who ate the last cookie? It was the dog!"; 
+  
+}
+​
+//If input is a string, return "Who ate the last cookie? It was Zach!""
+​
+// If the input is a float or an interger, return Who ate the last cookie? It was Monica.
+​
+//If input is else, retun "who ate the last cookie? It was the dog!"
+​
+​
