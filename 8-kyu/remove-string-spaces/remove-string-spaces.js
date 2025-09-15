a3413a59c56fa7093659c10e1907d91bf9d87185
Marjorie Alouidor <alouidor.m@gmail.com>
2025-09-15 01:49:58 -0400

    Add solution - CodeHub

---
+++ 8-kyu/remove-string-spaces/remove-string-spaces.js
@@ -0,0 +1,12 @@
+function noSpace(x){
+  
+  return x.replaceAll(" ", "");
+​
+}
+​
+// recieving an input, removing spacing in string. return the resultant string.
+​
+//string.protype.replace()
+​
+​
+​
